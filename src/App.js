bf84320d2dee07917e81fb96eeeeca63ee301a1e
hollymsnell <hollymsnell@gmail.com>
2022-12-08 04:37:41 -0700

    working code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <About />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h3>About</h3>
+      <h3>Interests</h3>
+    </div>
+  );
+}
