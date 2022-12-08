8cd0714d78e9b221a6f247d927ef891dbf356e36
hollymsnell <hollymsnell@gmail.com>
2022-12-08 05:17:00 -0700

    added social network links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,23 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="footer">social tags</footer>;
+      <ul class="icons">
+        <li>
+          <a href="https://www.linkedin.com/in/hollymsnell/" class="icon brands fa-linkedin">
+            <span class="label">LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/hollymsnell" class="icon brands fa-github">
+            <span class="label">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:hollymsnell@gmail.com" class="icon solid alt fa-envelope">
+            <span class="label">Email</span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
